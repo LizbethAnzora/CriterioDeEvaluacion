18625e3a959e8051fbeb7412dbe64cd64b6e4fc1
Josue <josueadolfov4lle@gmail.com>
2025-06-02 16:11:37 -0600

    Se trabajo en la clase Producto

--- Producto.java
+++ Producto.java
@@ -1,3 +1,61 @@
-public class Producto {
 
+// Clase Producto con encapsulamiento de propiedades
+class Producto {
+    private int id;
+    private String nombre;
+    private String marca;
+    private double precio;
+
+    // Constructor vacío
+    public Producto() { }
+
+    // Constructor parametrizado
+    public Producto(int id, String nombre, String marca, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // toString para mostrar datos del producto
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               ", Nombre: " + nombre +
+               ", Marca: " + marca +
+               ", Precio: $" + precio;
+    }
 }
